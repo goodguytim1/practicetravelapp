6ca0d34f28f2e0b117dbdb4834f15cd1393bfe5a
goodguytim1 <timobrien56@knights.ucf.edu>
2022-07-21 17:55:18 -0700

    i had the wrong server

---
+++ Server/controllers/user.js
@@ -0,0 +1,27 @@
+import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+export const  register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body
+        if(password.length < 6) return res.status(400).json({success: false, message:'Password must be 6 characters or more'})
+        const emailLowerCase = email.toLowerCase()
+        const existedUser = await User.findOne({email: emailLowerCase})
+        if(existedUser){
+            return res.status(400).json({success:false, message: 'User already exists!'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = await User.create({
+            name,
+            email: emailLowerCase,
+            password:hashedPassword
+        })
+        const {_id:id, photoURL} = user
+        const token = jwt.sign({id, name, photoURL}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        res.status(201).json({success: true, result: {id, name, email: user.email, photoURL, token}})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({sucess: false, message: "Something went wrong try again later"})
+    }
+}
